f84ca487a19b4db7f9ac9d49825c902f499b251d
Amir Abu Shamala <am1re344@gmail.com>
2022-09-12 17:54:11 +0300

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   resources :trees do
     resources :events
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'trees#index'
 end
